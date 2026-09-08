9d2a80af297831cabeb81f5470011d130b980437
intween <develop.intw@gmail.com>
2026-09-08 10:00:13 +0900

    ico 추가

--- app/layout.jsx
+++ app/layout.jsx
@@ -16,19 +16,10 @@ export const metadata = {
   icons: {
     icon: [
       {
-        url: '/icon-light-32x32.png',
-        media: '(prefers-color-scheme: light)',
-      },
-      {
-        url: '/icon-dark-32x32.png',
-        media: '(prefers-color-scheme: dark)',
-      },
-      {
         url: '/icon.ico',
         type: 'image/x-icon',
       },
     ],
-    apple: '/apple-icon.png',
   },
 };
 
